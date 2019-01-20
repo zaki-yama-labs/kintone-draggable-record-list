0a31671d5d53c49fa1ac187a2a527394be8967e1
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-01-21 06:24:17 +0900

    Add DynamicTable and sample-data

---
+++ src/DynamicTable.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Button from '@atlaskit/button';
+import DynamicTable from '@atlaskit/dynamic-table';
+import { caption, head, rows } from './content/sample-data';
+
+type State = { showPagination: boolean };
+
+export default class extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      showPagination: true,
+    };
+  }
+
+  togglePagination = () => {
+    this.setState({
+      showPagination: !this.state.showPagination,
+    });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <p>
+          Pagination is enabled or disabled by setting or unsetting the{' '}
+          <code>rowsPerPage</code>
+          prop.
+        </p>
+        <Button onClick={this.togglePagination}>Toggle pagination</Button>
+        <DynamicTable
+          caption={caption}
+          head={head}
+          rows={rows.slice(0, 5)}
+          rowsPerPage={this.state.showPagination ? 10 : undefined}
+          defaultPage={1}
+          isFixedSize
+          defaultSortKey="term"
+          defaultSortOrder="ASC"
+          onSort={() => console.log('onSort')}
+          onSetPage={() => console.log('onSetPage')}
+        />
+      </Wrapper>
+    );
+  }
+}
